597ac95da2ece0b8cc9ed4a7b80d02d74b092022
Parkhi <Parkhigoyal46@gmail.com>
2023-04-13 07:43:14 +0530

    created class for tutorials model

--- website-main/tutorials/models/models.py
+++ website-main/tutorials/models/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.urls import reverse
+
+class CreateLink(models.Model):
+
+    # dummy fields
+    link=models.CharField(max_length=12)
+    no_of_submissions= models.IntegerField(blank=False, null=True)
+
+
+    # method returning URL
+    def get_absolute_url(self):
+        return reverse('tutorials-home')
