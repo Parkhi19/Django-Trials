1a1eb0259fda0bb50b35c580ba5e3e08447f8ef1
Parkhi <Parkhigoyal46@gmail.com>
2023-04-16 07:33:47 +0530

    added fields to tutorials

--- website-main/tutorials/models/createLink.py
+++ website-main/tutorials/models/createLink.py
@@ -6,7 +6,8 @@ class CreateLink(models.Model):
     # dummy fields
     link=models.CharField(max_length=12)
     no_of_submissions= models.IntegerField(blank=False, null=True)
-
+    assignment_name = models.CharField(max_length=50)
+    course_name = models.CharField(max_length=50)
 
     # method returning URL
     def get_absolute_url(self):
